9d252f211a5bcd997749b8df031c560b1cdc44cf
Elijah Brown <elijah@ad7zj.net>
2024-10-31 15:32:03 -0700

    Removed some unneccesary code

--- exercise-001-corpus/word_count.go
+++ exercise-001-corpus/word_count.go
@@ -16,14 +16,6 @@ func main() {
 	// Get the file name from command-line arguments
 	filename := os.Args[1]
 
-	//open file
-	file, err := os.Open(filename)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	defer file.Close()
-
 	// Reads the entire file content into a []byte
 	content, err := os.ReadFile(filename)
 	if err != nil {
@@ -32,7 +24,6 @@ func main() {
 	}
 
 	histogram := corpus.Analysis(string(content))
-	//histogram = corpus.Analysis("Are you serious? I knew you were.")
 
 	for _, keyVal := range histogram {
 		if len(keyVal.Word) >= 8 {
