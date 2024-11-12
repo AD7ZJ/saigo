8d9e3ff7ed2469d1065ad420d05b62159e90988d
Elijah Brown <elijah@ad7zj.net>
2024-11-12 11:08:54 -0700

    Worked through excercise-008

--- exercise-008-iface/exhibit-c/shape.go
+++ exercise-008-iface/exhibit-c/shape.go
@@ -45,6 +45,47 @@ func (c *Circle) Area() float64 {
 	return math.Pi * c.radius * c.radius
 }
 
+//////////////
+// Triangle //
+//////////////
+
+type Triangle struct {
+	side float64
+}
+
+func (t *Triangle) Name() string {
+	return "Triangle"
+}
+
+func (t *Triangle) Perimeter() float64 {
+	return 3 * t.side
+}
+
+func (t *Triangle) Area() float64 {
+	return 0.5 * t.side * t.side
+}
+
+/////////////
+// Hexagon //
+/////////////
+
+type Hexagon struct {
+	side float64
+}
+
+func (h *Hexagon) Name() string {
+	return "Hexagon"
+}
+
+func (h *Hexagon) Perimeter() float64 {
+	return 6 * h.side
+}
+
+// Area calculates the area of the hexagon using the formula (3 * √3 / 2) * side ^ 2
+func (h *Hexagon) Area() float64 {
+	return (3 * math.Sqrt(3) / 2) * h.side * h.side
+}
+
 ////////////////
 // Efficiency //
 ////////////////
@@ -64,10 +105,53 @@ func Efficiency(s Shape) {
 	fmt.Printf("Efficiency of a %s is %f\n", name, efficiency)
 }
 
-func main() {
-	s := Square{side: 10.0}
-	Efficiency(&s)
+// Build function to create an instance of type Shape. Every return value should implement the Shape interface
+func Build(shape string, parameters ...float64) Shape {
+	switch shape {
+	case "Triangle":
+		if len(parameters) < 1 {
+			fmt.Println("Not enough parameters for Triangle")
+			return nil
+		}
+		return &Triangle{side: parameters[0]}
 
-	c := Circle{radius: 10.0}
-	Efficiency(&c)
+	case "Square":
+		if len(parameters) < 1 {
+			fmt.Println("Not enough parameters for Square")
+			return nil
+		}
+		return &Square{side: parameters[0]}
+
+	case "Hexagon":
+		if len(parameters) < 1 {
+			fmt.Println("Not enough parameters for Hexagon")
+			return nil
+		}
+		return &Hexagon{side: parameters[0]}
+
+	case "Circle":
+		if len(parameters) < 1 {
+			fmt.Println("Not enough parameters for Circle")
+			return nil
+		}
+		return &Circle{radius: parameters[0]}
+
+	default:
+		fmt.Println("Unknown shape type")
+		return nil
+	}
+}
+
+func main() {
+	//s := Square{side: 10.0}
+	Efficiency(Build("Square", 10.0))
+
+	//c := Circle{radius: 10.0}
+	Efficiency(Build("Circle", 10.0))
+
+	//t := Triangle{side: 10.0}
+	Efficiency(Build("Triangle", 10.0))
+
+	//h := Hexagon{side: 10.0}
+	Efficiency(Build("Hexagon", 10.0))
 }
