7841b4bf7f6f9a89bec8bf7d0864ca6ef3372673
Elijah Brown <elijah@ad7zj.net>
2024-11-13 09:48:25 -0700

    Updated some comments

--- exercise-009-rock/src/rock/rock.go
+++ exercise-009-rock/src/rock/rock.go
@@ -5,7 +5,9 @@ func main() {
 	// Create Game
 	game := &Game{}
 
-	// Add Players
+	// Add Players. The syntax here is a bit odd, since Add() is defined to take a Player type, not a Player pointer type.
+	// But since the interface is using pointer reveivers on the Type() and Play() function, we need to pass in a
+	// pointer here. Go allows a lot of leeway in matching pointers vs the dereferenced value.
 	game.Add(&RandoRex{})
 	game.Add(&Obsessed{})
 	game.Add(&Flipper{})
